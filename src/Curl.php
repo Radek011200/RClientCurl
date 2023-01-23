b228c2f15977126a0483d9e26d051e58e252ef92
Radek011200 <beziek120@o2.pl>
2023-01-23 17:32:29 +0100

    add exceptions

--- src/Curl.php
+++ src/Curl.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Radek011200\CurlClientPhp;
 
+use Exception;
 use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -15,24 +16,26 @@ class Curl
     /**
      * @param RequestInterface $request
      * @return ResponseInterface
+     * @throws Exception
      */
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        $handle = curl_init();
-        curl_setopt($handle, CURLOPT_URL, (string)$request->getUri());
-        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, $request->getMethod());
-        curl_setopt($handle, CURLOPT_HTTPHEADER, $request->getHeaders());
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $request->getBody());
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($handle, CURLOPT_HEADER, true);
-        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
-        $response = curl_exec($handle);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, (string)$request->getUri());
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $request->getMethod());
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $request->getHeaders());
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request->getBody());
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        $response = curl_exec($curl);
 
-        $header_size = curl_getinfo($handle, CURLINFO_HEADER_SIZE);
+
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
         $headers = substr($response, 15, $header_size-15);
 
         $body = substr($response, $header_size);
-        $status = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 
         $header_rows = explode("\n", $headers);
@@ -47,7 +50,10 @@ class Curl
             $headers[$key] = $value;
         }
 
-        return new Response($status, $headers, $body);
+        $response = new Response($status, $headers, $body);
+        $this->checkResponse($response);
+
+        return $response;
     }
 
     /**
@@ -75,6 +81,7 @@ class Curl
      * @param string $url
      * @param array $options
      * @return ResponseInterface
+     * @throws Exception
      */
     public function sendGet(string $url, array $options = []): ResponseInterface
     {
@@ -84,5 +91,17 @@ class Curl
 
         $request = $this->prepareRequest($request, $options);
         return $this->sendRequest($request);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkResponse(ResponseInterface $response): void
+    {
+        $status = $response->getStatusCode();
+        $toCheck = $status/100;
+
+        if($toCheck >= 4 && $toCheck < 6)
+            throw new Exception($status . " " . $response->getReasonPhrase());
     }
 }
